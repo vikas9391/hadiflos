b188965e6e5e0827f8390cf51e68c40330ee3819
Gummadala vikas <vikas93912@gmail.com>
2026-05-02 21:30:25 +0530

    database connected

--- djproject/hadifloscom/settings.py
+++ djproject/hadifloscom/settings.py
@@ -3,6 +3,8 @@ Django settings for HadiFlosCom – Debt Recovery Platform
 """
 from pathlib import Path
 import os
+import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -53,11 +55,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hadifloscom.wsgi.application'
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
